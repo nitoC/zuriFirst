60b35991ea1e542250e8675da8b8007818633f43
nitoc <daren2track@gmail.con>
2022-10-29 11:02:24 +0100

    refactor(social):add social link

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,10 +3,10 @@ function Social() {
   return (
     <div className="socials">
       <div>
-       <a href="https://hng9.slack.com"><img id="slack__icon" src="/icons/slack.svg" alt="" /></a>
+       <a href="https://hng9.slack.com" rel='noreferrer' ><img id="slack__icon" src="/icons/slack.svg" alt="" /></a>
       </div>
       <div>
-        <a href="https://github.com/nitoC"><img src="/icons/Socialicon.svg" id="social__icon" alt="" /></a>
+        <a target='_blank' rel='noreferrer' href="https://github.com/nitoC"><img src="/icons/Socialicon.svg" id="social__icon" alt="" /></a>
       </div>
     </div>
   )
